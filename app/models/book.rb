e0851213a9b8f82316229397b3c8b15e7338a2dc
Glaubenio Patricio <glaubeniopatricio@yahoo.com.br>
2021-01-29 09:20:54 -0300

    Sets up project structure.

---
+++ app/models/book.rb
@@ -0,0 +1,10 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Book < ApplicationRecord
+end
