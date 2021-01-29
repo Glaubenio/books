15c4e87e4e638bf14e60fc851a97cd97bbbbf29b
Glaubenio Patricio <glaubeniopatricio@yahoo.com.br>
2021-01-29 14:54:43 -0300

    Fix description field type and some translation errors.

--- db/migrate/20210129121003_create_books.rb
+++ db/migrate/20210129121003_create_books.rb
@@ -2,7 +2,7 @@ class CreateBooks < ActiveRecord::Migration[5.1]
   def change
     create_table :books do |t|
       t.string :title, null: false
-      t.string :description, null: false
+      t.text :description, null: false
       t.string :image, null: false
       t.string :author, null: false
       t.timestamps
